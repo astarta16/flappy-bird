10a2e293b742749b6c345886b4ce11d9b4b55476
astarta16 <ngedeshuri2001@gmail.com>
2024-07-18 14:28:09 +0400

    add pen

--- main.py
+++ main.py
@@ -10,3 +10,12 @@ wn.setup(width=500, height=800)
 wn.tracer(0)
 
 wn.register_shape("bird.gif")
+
+
+pen = turtle.Turtle()
+pen.speed(0)
+pen.hideturtle()
+pen.penup()
+pen.color("white")
+pen.goto(0, 250)
+pen.write("0", move=False, align="left", font=("Arial", 32, "normal"))
