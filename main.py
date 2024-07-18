6f28c6c8db190e02f5e0abdd2458744f4437ddbe
astarta16 <ngedeshuri2001@gmail.com>
2024-07-18 14:27:02 +0400

    add turle library

---
+++ main.py
@@ -0,0 +1,10 @@
+
+import turtle
+import time
+
+wn = turtle.Screen()
+wn.title("Flappy Bird by @TokyoEdTech")
+wn.bgcolor("blue")
+wn.bgpic("background.gif")
+wn.setup(width=500, height=800)
+wn.tracer(0)
